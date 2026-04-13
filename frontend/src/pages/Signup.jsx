81b83773bb8573999807e2493a464d997678e262
vedangp1998 <vedang.prajapati18@gmail.com>
2026-04-14 01:23:33 +0530

    working on signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="h-screen w-full flex items-center justify-center flex-col gap-4">
+      <div className="flex items-center justify-center flex-col">
+        <div className="text-4xl font-bold text-gray-800">
+          Create You Account
+        </div>
+        <p className="text-lg text-gray-400">
+          This is the Authentication Application signup page !
+        </p>
+      </div>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign up</CardTitle>
+          <CardDescription>
+            Enter your details below to signup to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">User Name</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="enter your username"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
