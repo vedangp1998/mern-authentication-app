35d0ca499ef3292100e4769f35054a8e272d79d3
vedangp1998 <vedang.prajapati18@gmail.com>
2026-04-13 23:48:52 +0530

    setup react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 const App = () => {
   return (
     <>
-      <h1>Welcome</h1>
+      <div>
+        <RouterProvider router={router} />
+      </div>
     </>
   );
 };
