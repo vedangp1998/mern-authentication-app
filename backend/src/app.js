1a76614d603f465d546b3b87d9ee58bdff49aac2
vedangp1998 <vedang.prajapati18@gmail.com>
2026-04-14 14:23:29 +0530

    signup api integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,22 @@
-const express = require("express");
-const connectDB = require("./db/db");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require('express');
+const connectDB = require('./db/db');
+const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const userRoutes = require("./routes/user.route");
+const userRoutes = require('./routes/user.route');
 
 const app = express();
 
 connectDB();
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use("/user", userRoutes);
+app.use('/user', userRoutes);
 
 module.exports = app;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
@@ -10,35 +9,84 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { EyeOff, Eye, Loader2 } from 'lucide-react';
+import { useState, useRef } from 'react';
+import axios from 'axios';
+import { toast } from 'sonner';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const formData = {
+      username: usernameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    try {
+      const res = await axios.post(
+        'http://localhost:3000/user/register',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      if (res.data.success) {
+        navigate('/login');
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setTimeout(() => setIsLoading(false), 2000);
+
+    usernameRef.current.value = '';
+    emailRef.current.value = '';
+    passwordRef.current.value = '';
+  };
+
   return (
     <div className="h-screen w-full flex items-center justify-center flex-col gap-4">
       <div className="flex items-center justify-center flex-col">
         <div className="text-4xl font-bold text-gray-800">
-          Create You Account
+          Create Your Account
         </div>
         <p className="text-lg text-gray-400">
-          This is the Authentication Application signup page !
+          This is the Authentication Application signup page!
         </p>
       </div>
       <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle className="text-xl">Sign up</CardTitle>
           <CardDescription>
-            Enter your details below to signup to your account
+            Enter your details below to sign up to your account
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form id="signup-form" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
-                <Label htmlFor="email">User Name</Label>
+                <Label htmlFor="username">User Name</Label>{' '}
                 <Input
                   id="username"
                   type="text"
                   placeholder="enter your username"
                   required
+                  ref={usernameRef}
                 />
               </div>
               <div className="grid gap-2">
@@ -48,29 +96,53 @@ const Signup = () => {
                   type="email"
                   placeholder="m@example.com"
                   required
+                  ref={emailRef}
                 />
               </div>
               <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                <Label htmlFor="password">Password</Label>
+                <div className="relative">
+                  <Input
+                    id="password"
+                    type={showPassword ? 'text' : 'password'}
+                    required
+                    ref={passwordRef}
+                  />
+                  <Button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    variant="ghost"
+                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
                   >
-                    Forgot your password?
-                  </a>
+                    {showPassword ? (
+                      <EyeOff className="w-6 h-6" />
+                    ) : (
+                      <Eye className="w-6 h-6" />
+                    )}
+                  </Button>
                 </div>
-                <Input id="password" type="password" required />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            Login
+          <Button
+            type="submit"
+            form="signup-form"
+            className="w-full"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Creating
+                Account...
+              </>
+            ) : (
+              'Sign up'
+            )}
           </Button>
           <Button variant="outline" className="w-full">
-            Login with Google
+            Sign up with Google
           </Button>
         </CardFooter>
       </Card>
